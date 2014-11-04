1b6f6765c0fa7fd4a29df1c2f6684b4fbf9d982e
Hayri <hayri@hoshilab.com>
2014-11-04 11:37:10 +0100

    customize admin for Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['title', 'description']
+  list_filter = ['published', 'created']
+  search_fileds = ['title','description','content']
+  date_hierarchy = 'created'
+  save_on_top = True
+  prepopulated_fields = {"slug":("title",)}
+
+admin.site.register(Post, PostAdmin)
