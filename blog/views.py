f04705f300a97d25b9f6dcdd8418468ba6a31c2c
Hayri <hayri@hoshilab.com>
2014-11-04 11:41:47 +0100

    the index and show view added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+  posts = Post.objects.filter(published=True)
+  
+  return render(request, 'blog/index.html', {'posts': posts})
+
+def post(request, slug):
+  post = get_object_or_404(Post, slug=slug)
+
+  return render(request, 'blog/post.html', {'post': post})
+
--- netmag/urls.py
+++ netmag/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'netmag.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blog.views.index'),
+    url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
 )
